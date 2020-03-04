ed11747e6e5772d3b4b3da0b3ef63ee46c4835f1
RK <r.nakamura0316@gmail.com>
2020-03-04 23:12:30 +0900

    initial

---
+++ c3pro/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <chart v-for="type in chartTypeList" v-bind:key="type" @emphasize="emphasize" v-bind:chart-type="type" v-bind:chart-data="chartData" v-bind:emphasize-data-name="emphasizeDataName" v-bind:x-ticks="xTicks"></chart>
+  </div>
+</template>
+
+<script>
+import Chart from './components/Chart.vue'
+//import axios from '../node_modules/axios'
+
+export default {
+  name: 'App',
+  components: {
+    Chart
+  },
+  // htmlがコンポーネントが参照するプロパティ
+  data: () => ({
+    // 表示するチャートの型を指定
+    chartTypeList: ["line", "bar", "pie"],
+    // chartData: [["x", 0, 20, 10, 40, 15, 500], ["y", 30, 200, 100, 400, 150, 250]],
+    // ["宇都宮", 516546, 518398, 519904, 521082, 521702, 521556], ["埼玉", 7209945, 7223317, 7239813, 7270549, 7291207, 7310697]
+    chartData: [["宇都宮", 516546, 518398, 519904, 521082, 521702, 521556], ["ダミー", 506546, 508398, 509904, 511082, 511702, 521556]],
+    xTicks: ["H25", "H26", "H27", "H28", "H29", "H30"],
+    emphasizeDataName: ""
+  }),
+  mounted() {
+    // TODO データをサーバから取得する
+    // axios.defaults.baseURL = 'http://localhost:8080';
+    // axios.defaults.headers.get['Content-Type'] = 'application/json;charset=utf-8';
+    // axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
+    // axios
+    //   .get('https://www.google.com/')
+    //   .then(() => this.chartData.push(["x", 0, 20, 10, 40, 15, 500], ["y", 30, 200, 100, 400, 150, 250]))
+    //   .catch(() => this.chartData.push(["x", 0, 20, 10, 40, 15, 500], ["y", 30, 200, 100, 400, 150, 250]))
+  },
+  methods: {
+    emphasize(name) {
+      // 子から通知されたデータ名を保持する
+      // ここで値を変更することで、強調表示する値を子へ通知する
+      if (this.emphasizeDataName === name) {
+        this.emphasizeDataName = "";
+      } else {
+        this.emphasizeDataName = name;
+      }
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ c3pro/src/components/Chart.vue
@@ -0,0 +1,121 @@
+<template>
+  <div>
+    <vue-c3 :handler="handler"></vue-c3>
+  </div>
+</template>
+
+<script>
+// hander のために必要
+import Vue from "vue";
+
+// メインのコンポーネント
+import VueC3 from "vue-c3";
+
+// CSS を読み込む
+import "c3/c3.min.css";
+
+export default {
+  name: "demo",
+  components: {
+    VueC3
+  },
+  // htmlがコンポーネントが参照するプロパティ
+  data: () => ({
+    handler: new Vue(),
+  }),
+  props: ["chartType", "chartData", "emphasizeDataName", "xTicks"],
+  computed: {
+    // c3 のオプションに渡す値
+    // ここでC3特有の値を渡す
+    options() {
+      return {
+        // 主としてデータ
+        data: {
+          type: this.chartType,
+          columns: this.chartData,
+          // columns: [
+          //   // 配列一つ目が、データの名前になる
+          //   ["x", 0, 20, 10, 40, 15, this.val],
+          //   ["y", 30, 200, 100, 400, 150, 250]
+          // ]
+          // selection: {
+          //   enabled: true
+          // },        
+          onclick: (d, element) => {
+            // console.log(d);
+            // console.log(element);
+            this.onPlotClick(d, element);
+          }
+        },
+        axis: {
+          x: {
+            label: {
+              // X 軸のラベルを変更する
+              // 位置も変えれる
+              text: "年",
+              position: "outer-center"
+            },
+            type: "category",
+            categories: this.xTicks
+          },
+          y: {
+            label: {
+              text: "人口数",
+              position: "outer-middle"
+            }
+          }
+        },
+        // tooltip: {
+        //   // 各点にマウスオーバーした際に出てくる部分の設定
+        //   format: {
+        //     title(d) {
+        //       return `第 ${d} 期`;
+        //     },
+        //     value(value, ratio, id) {
+        //       return `${value} 億円`;
+        //     }
+        //   }
+        // },
+        grid: {
+          x: {
+            show: true
+          },
+          y: {
+            show: true
+          }
+        },
+        legend: {
+          position: "right"
+        }
+      };
+    }
+  },
+  mounted() {
+    // DOMが全部作られてからコールされる
+    // optionsで定義した値をc3へ渡す
+    this.handler.$emit("init", this.options);
+  },
+  methods: {
+    onPlotClick: function(d) {
+      // 選択されたプロットのデータ名を親へ通知する
+      this.$emit('emphasize', d.name);
+    }
+  },
+  watch: {
+    // 親から渡された値が更新されたときに呼び出される
+    emphasizeDataName: function(newVal) {
+      this.handler.$emit('dispatch', (chart) => chart.focus(newVal));
+    }
+  }
+};
+</script>
+<style scoped>
+.button-wrapper {
+  text-align: center;
+}
+
+button {
+  padding: 10px;
+  background: red;
+}
+</style>
